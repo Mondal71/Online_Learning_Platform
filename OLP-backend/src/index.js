75afeeafb660e9293ae00ed805c834493d5e192d
Mondal71 <mondalasp123@gmail.com>
2025-09-29 15:56:56 +0530

    utils setup

--- OLP-backend/src/index.js
+++ OLP-backend/src/index.js
@@ -1,9 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js"
+import { app } from "./app.js";
 // .env load
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+    console.log(`server is running at port : ${process.env.PORT}`);
+  })
+})
+  .catch((err) => {
+  console.log("MongoDB connection failed !!", err);
+})
 
